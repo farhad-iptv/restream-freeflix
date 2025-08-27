c329bc67ed40d22dfd922993e5bc002f8b64743c
farhad-iptv <hossainfarhad491@gmail.com>
2025-08-27 19:33:44 +0600

    Update [id].js

--- api/filex/[id].js
+++ api/filex/[id].js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     id = String(id).replace(/\.m3u8$/i, "");
 
     // Xtream Codes base URL (your source)
-    const baseXtream = "http://filex.tv:8080/live/Multicity/1234/110038";
+    const baseXtream = "http://filex.tv:8080/live/Multicity/1234";
     const sourceUrl = `${baseXtream}/${id}.m3u8`;
 
     // Fetch upstream with curl-like headers
