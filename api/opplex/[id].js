b2434436a2731a8d3e9a3d45d3da02801adb182d
farhad-iptv <hossainfarhad491@gmail.com>
2025-09-06 22:34:37 +0600

    Update [id].js

--- api/opplex/[id].js
+++ api/opplex/[id].js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     id = String(id).replace(/\.m3u8$/i, "");
 
     // Xtream Codes base URL (your source)
-    const baseXtream = "http://opplex.tv:8080/live/1122/2211";
+    const baseXtream = "http://opplex.tv:8080/live/2464/4642";
     const sourceUrl = `${baseXtream}/${id}.m3u8`;
 
     // Fetch upstream with curl-like headers
